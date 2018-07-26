db3550bb87fbc48734e66cd790b51936cbfd74de
Your Name <alfredoeka@outlook.com>
2018-07-26 13:03:13 +0700

    Add qna

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/faq', function () {
+    return view('faq');
+});
+
 
 Route::get('test/spreadsheet', 'MutationFormController@make');
 
